629929c00229c1237d98e3404d9c1254045847f5
EduardoGrangeiro2002 <eduatdoaugusto503@gmail.com>
2022-08-12 12:21:35 -0300

    Testando  a criação de usuários

--- test/rockelivery/users/create_test.exs
+++ test/rockelivery/users/create_test.exs
@@ -1,7 +1,29 @@
 defmodule Rockelivery.Users.CreateTest do
-  alias Rockelivery.{Error, User}
+  use Rockelivery.DataCase, async: true
 
   import Rocklivery.Factory
 
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
 
+   describe "call/1" do
+     test "when all params are valids, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, age: 21, email: "edu.grangeiro2002@gmail.com"}} = response
+     end
+
+     test "when there are invalid params, returns an error" do
+      params = build(:user_params, %{password: 123, age: 13})
+
+      response = Create.call(params)
+
+      expected_response = %{age: ["must be greater than or equal to 18"], password: ["is invalid"]}
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected_response
+     end
+   end
 end
